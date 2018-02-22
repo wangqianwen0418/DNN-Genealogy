55f1ed1a7176a40e2fb4aef7a7e8c83d17f70470
wangqianwen0418 <wangqianwen0418@163.com>
2018-02-22 19:34:35 +0800

    add: dag transition animation

--- src/components/Evolution3.tsx
+++ src/components/Evolution3.tsx
@@ -20,12 +20,12 @@ export interface Props {
 const appData = [
     {
         label: "sequence data",
-        key:"sequence data",
+        key: "sequence data",
         value: "0-0"
     }, {
         label: "nonsequence data",
         value: "nonsequence data",
-        key:"2"
+        key: "2"
     }
 ]
 
@@ -40,13 +40,13 @@ export interface State {
     w: number | undefined,
     appValue: string | undefined,
     // appData: any,
-    topDoi:Node[],
-    topParent: Node|undefined,
-    topChild: Node|undefined
+    topDoi: Node[],
+    topParent: Node | undefined,
+    topChild: Node | undefined
 }
-const margin = 30, nodeH = 20, nodeW = 100, labelL=8,
-    expandH = 300,expandW=400,
-    r_api = 1, r_dist = -100, r_diff=0.01 //factors for DOI calculation
+const margin = 30, nodeH = 20, nodeW = 100, labelL = 8,
+    expandH = 300, expandW = 400,
+    r_api = 1, r_dist = -100, r_diff = 0.01 //factors for DOI calculation
 export default class Evolution extends React.Component<Props, State>{
     constructor(props: Props) {
         super(props)
@@ -60,7 +60,7 @@ export default class Evolution extends React.Component<Props, State>{
             h: 0,
             appValue: undefined,
             // appData: [],
-            topDoi:[],
+            topDoi: [],
             topChild: undefined,
             topParent: undefined
         }
@@ -75,7 +75,7 @@ export default class Evolution extends React.Component<Props, State>{
 
             let pub_date = moment(d.date, 'YYYY-MM-DD'),
                 dif = moment().diff(pub_date, "months")
-            d.api = d.citation/dif
+            d.api = d.citation / dif
         })
         let { nodes, edges, width: w, height: h, topDoi } = this.getDag(datum)
 
@@ -91,12 +91,12 @@ export default class Evolution extends React.Component<Props, State>{
         // }
 
         // label(appData)
-        
 
-        this.setState({ nodes, edges, w, h,  datum, topDoi })
+
+        this.setState({ nodes, edges, w, h, datum, topDoi })
     }
-    getDag(datum: NN[], selectedNode:Node|undefined=undefined) {
-        let selectedID = selectedNode?selectedNode.ID:undefined
+    getDag(datum: NN[], selectedNode: Node | undefined = undefined) {
+        let selectedID = selectedNode ? selectedNode.ID : undefined
         let dag = new dagre.graphlib.Graph();
         dag.setGraph({
             ranksep: nodeW * 1,
@@ -108,16 +108,16 @@ export default class Evolution extends React.Component<Props, State>{
             // ranker: "longest-path"
         });
         dag.setDefaultEdgeLabel(() => { return {}; });
-        
+
 
         datum.forEach((node: NN) => {
             // let label = `${layer.name}:${layer.class_name}`
-            let selected:boolean = (node.ID==selectedID)
-            dag.setNode(node.ID, 
-                { 
-                    label: node.ID, 
-                    width: selected?expandW:nodeW, 
-                    height: selected?expandH:nodeH,
+            let selected: boolean = (node.ID == selectedID)
+            dag.setNode(node.ID,
+                {
+                    label: node.ID,
+                    width: selected ? expandW : nodeW,
+                    height: selected ? expandH : nodeH,
                     ID: node.ID,
                     api: node.api
                 }
@@ -126,9 +126,9 @@ export default class Evolution extends React.Component<Props, State>{
             if (node.parents.length > 0) {
                 node.parents.forEach((parent: any) => {
                     dag.setEdge(
-                        parent.ID, 
-                        node.ID, 
-                        { 
+                        parent.ID,
+                        node.ID,
+                        {
                             label: parent.link_info,
                             from: parent.ID,
                             to: node.ID
@@ -137,166 +137,168 @@ export default class Evolution extends React.Component<Props, State>{
                 })
             }
         })
-        
 
-        
 
-        const getEdgeWeight = (e:dagre.Edge)=>dag.node(e.v).api+dag.node(e.w).api
+
+
+        const getEdgeWeight = (e: dagre.Edge) => dag.node(e.v).api + dag.node(e.w).api
         // const getEdgeWeight = (e:dagre.Edge)=>1
-        const getEI = (v:dagre.Edge)=>1
-        let distanceDict:any
-        if(selectedNode){
+        const getEI = (v: dagre.Edge) => 1
+        let distanceDict: any
+        if (selectedNode) {
             distanceDict = dagre.graphlib.alg
-                                .dijkstra(dag, selectedNode.label, (e)=>1, v=>dag.nodeEdges(v))            
+                .dijkstra(dag, selectedNode.label, (e) => 1, v => dag.nodeEdges(v))
         }
         // let tree = dagre.graphlib.alg.prim(dag, getEdgeWeight)
         // console.info(tree.edges)
-        
+
 
         //calculate doi for each node
         dag.nodes().forEach((v) => {
             if (dag.node(v)) {
-                let node:Node = dag.node(v),
-                distance = selectedNode?distanceDict[v].distance:0
+                let node: Node = dag.node(v),
+                    distance = selectedNode ? distanceDict[v].distance : 0
                 node.api_diff = Math.max(
-                    node.api||0,
-                    Math.max(...(dag.neighbors(v)||[]).map((neighbor:Node)=>{
-                        return r_diff * (neighbor.api||0)/getEI({v, w: neighbor.label})
-                    })) 
+                    node.api || 0,
+                    Math.max(...(dag.neighbors(v) || []).map((neighbor: Node) => {
+                        return r_diff * (neighbor.api || 0) / getEI({ v, w: neighbor.label })
+                    }))
                 )
-                node.doi = node.api_diff + r_dist*distance
+                node.doi = node.api_diff + r_dist * distance
             }
         })
         dag.edges().forEach((e, i) => {
-            let edge:GraphEdge = dag.edge(e)
+            let edge: GraphEdge = dag.edge(e)
             edge.weight = getEdgeWeight(e)
         });
 
         //calculate the top N doi nodes, and update their size
-        let topParent:Node|undefined = undefined
-        let topChild:Node|undefined = undefined
-        if(selectedNode){
+        let topParent: Node | undefined = undefined
+        let topChild: Node | undefined = undefined
+        if (selectedNode) {
             let parents = dag.predecessors(selectedNode.label),
                 children = dag.successors(selectedNode.label)
-            if(parents&&parents.length!=0){
-                topParent = parents.map(v=>dag.node(v)).sort((a,b)=>b.doi-a.doi)[0]
-                dag.setNode(topParent.label, { 
-                    label: topParent.ID, 
-                    width: expandW, 
+            if (parents && parents.length != 0) {
+                topParent = parents.map(v => dag.node(v)).sort((a, b) => b.doi - a.doi)[0]
+                dag.setNode(topParent.label, {
+                    label: topParent.ID,
+                    width: expandW,
                     height: expandH,
                     ID: topParent.ID,
                     api: topParent.api
                 })
             }
-            if(children&&children.length!=0){
-                topChild = children.map(v=>dag.node(v)).sort((a,b)=>b.doi-a.doi)[0]
-                dag.setNode(topChild.label, { 
-                    label: topChild.ID, 
-                    width: expandW, 
+            if (children && children.length != 0) {
+                topChild = children.map(v => dag.node(v)).sort((a, b) => b.doi - a.doi)[0]
+                dag.setNode(topChild.label, {
+                    label: topChild.ID,
+                    width: expandW,
                     height: expandH,
                     ID: topChild.ID,
                     api: topChild.api
                 })
             }
-            
+
         }
 
-        const topN = (nodes:string[], n:number=3)=>{
-            let topDoi:Node[] = []
-            for(let i=0;i<nodes.length;i++){
+        const topN = (nodes: string[], n: number = 3) => {
+            let topDoi: Node[] = []
+            for (let i = 0; i < nodes.length; i++) {
                 let v = nodes[i]
                 let node = dag.node(v)
                 //exclude topParent and topChild from topN
-                if((topChild&&v==topChild.label)
-                    ||(topParent&&v==topParent.label)
-                    ||(selectedNode&&v==selectedNode.label)){                   
-                }else{
+                if ((topChild && v == topChild.label)
+                    || (topParent && v == topParent.label)
+                    || (selectedNode && v == selectedNode.label)) {
+                } else {
                     topDoi.push(node)
                 }
-                
-                if(topDoi.length>n){
-                    topDoi.sort((a,b)=>(b.doi||0)-(a.doi||0))
+
+                if (topDoi.length > n) {
+                    topDoi.sort((a, b) => (b.doi || 0) - (a.doi || 0))
                     topDoi.pop()
                 }
             }
             return topDoi
         }
-        let topDoi:Node[] = topN(dag.nodes())
+        let topDoi: Node[] = topN(dag.nodes())
 
 
         //calculate layout
         dagre.layout(dag)
 
-        let nodes:Node[] = dag.nodes().map(v=>dag.node(v)),
-            edges:GraphEdge[] = dag.edges().map(e=>dag.edge(e)),
+        let nodes: Node[] = dag.nodes().map(v => dag.node(v)),
+            edges: GraphEdge[] = dag.edges().map(e => dag.edge(e)),
             height = dag.graph().height,
             width = dag.graph().width
         return { nodes, edges, height, width, topDoi, topParent, topChild }
     }
     drawNodes(nodes: Node[]) {
-        let {selectedNode, topDoi} = this.state, 
-            selectedID = selectedNode?selectedNode.ID:undefined
+        let { selectedNode, topDoi } = this.state,
+            selectedID = selectedNode ? selectedNode.ID : undefined
         const menu = (
             <Menu >
                 <Menu.Item key="1">text intro</Menu.Item>
                 <Menu.Item key="2">compare performance</Menu.Item>
                 <Menu.Item key="3">detailed structure</Menu.Item>
             </Menu>
-            );
-            
+        );
+
         return (<g className="nodes" >
             {nodes.map((node: Node) => {
-                let selected:boolean = (node.ID===selectedID),
-                    isTop:boolean = topDoi.map(d=>d.ID).indexOf(node.ID)!=-1
+                let selected: boolean = (node.ID === selectedID),
+                    isTop: boolean = topDoi.map(d => d.ID).indexOf(node.ID) != -1
                 return <g key={node.label}
                     transform={`translate (${node.x - node.width / 2}, ${node.y - node.height / 2})`}
                     onClick={() => this.selectNode(node)}
-                    
+
                 >
-                    
+
                     <rect width={node.width} height={node.height}
-                    fill="transparent"
-                    stroke={selected?"red":(isTop?"blue":"gray")}
-                    strokeWidth={selected?2:(isTop?2:1)}
-                    cursor="pointer"
-                    ></rect>
-                    {node.height>nodeH?<foreignObject>
-                        <div style={{height:node.height}}>
-                            <img 
-                            className="abstract"
-                            src={`../../images/${node.label}.png`}
-                                //    height={node.height}
-                                    width={node.width}
-                                />
-                            </div>
-                            <Dropdown overlay={menu} className="infoButton">
-                            <Button>{node.label}</Button>
-                            </Dropdown>
-                        
-                    </foreignObject>
-                    :<text textAnchor="middle"
-                        fontSize={0.7*nodeH}
+                        className="Node"
+                        rx={2}
+                        ry={2}
+                        fill="transparent"
+                        stroke={selected ? "red" : (isTop ? "#7dc1f2" : "gray")}
+                        strokeWidth={selected ? 3 : (isTop ? 3 : 1)}
                         cursor="pointer"
-                        x={node.width / 2}
-                        y={node.height-0.1*nodeH}
+                    ></rect>
+                    <text textAnchor="middle"
+                            fontSize={0.7 * nodeH}
+                            cursor="pointer"
+                            x={node.width / 2}
+                            y={node.height - 0.1 * nodeH}
                         >
-                        {
-                             (node.label.length<labelL)?
-                             node.label:(node.label.slice(0, labelL)+'...')
-                        }                       
+                            {
+                                (node.label.length < labelL) ?
+                                    node.label : (node.label.slice(0, labelL) + '...')
+                            }
                     </text>
-                    }
-                    
+                    <foreignObject>
+                        <div style={{ height: node.height }}>
+                            <img
+                                className="abstract Node"
+                                src={`../../images/${node.label}.png`}
+                                //    height={node.height}
+                                width={node.width > nodeW ? node.width : 0}
+                            />
+                        </div>
+                        {node.height > nodeH ?
+                            <Dropdown overlay={menu} className="infoButton">
+                                <Button>{node.label}</Button>
+                            </Dropdown>
+                        : <span/>}
+                    </foreignObject>
                 </g>
             })}
         </g>)
     }
     oneEdge(edge: GraphEdge, i: number) {
         let { points, from, to, label } = edge,
-            {selectedNode} = this.state,
-            selectedID = selectedNode?selectedNode.label:undefined
-        
-        
+            { selectedNode } = this.state,
+            selectedID = selectedNode ? selectedNode.label : undefined
+
+
         // let len = points.length
         // if (len == 0) { return }
         // let start = `M ${points[0].x} ${points[0].y}`
@@ -309,15 +311,16 @@ export default class Evolution extends React.Component<Props, State>{
         // let pathData = `${start}  ${vias.join(' ')}`
         //change curve path to straight line
         let pathData = `M ${points[0].x} ${points[0].y} 
-        L ${points[points.length-1].x} ${points[points.length-1].y}`
-        let highlight:boolean = ((from==selectedID)||(to==selectedID))
+        L ${points[points.length - 1].x} ${points[points.length - 1].y}`
+        let highlight: boolean = ((from == selectedID) || (to == selectedID))
         return <g className='link' key={`${i}_${from}->${to}`}>
             <path
                 strokeLinecap="round"
                 d={pathData}
-                stroke={highlight?"gray":"gray"}
+                stroke={highlight ? "gray" : "gray"}
                 fill='none'
-                strokeWidth={highlight?2:1}
+                strokeWidth={highlight ? 2 : 1}
+                className="Edge"
             >
                 <title>
                     {label}
@@ -336,6 +339,15 @@ export default class Evolution extends React.Component<Props, State>{
     }
     componentWillMount() {
         this.getData()
+    }
+    componentDidMount() {
+
+        const zoomed = () => {
+            svg.attr("transform", d3.event.transform);
+        }
+        let svg = d3.select('svg')
+            .call(d3.zoom().on("zoom", zoomed));
+
     }
     onChange = (appValue: string) => {
         this.setState({ appValue });
@@ -369,7 +381,7 @@ export default class Evolution extends React.Component<Props, State>{
 
         // let ratio = Math.min(screen_w/(w||1), screen_h/(h||1))
         let { train, arc } = this.props
-        
+
         return <div className="Evolution View">
             <div style={{ position: "absolute", left: "20px", top: "20px" }}>
                 Training methods:{train}
@@ -389,10 +401,14 @@ export default class Evolution extends React.Component<Props, State>{
                 onChange={this.onChange}
             />
 
-            <svg width="100%" height="100%" 
-            viewBox={`0 0 ${w} ${h}`}
+            <svg
+                //alway show the whole dag
+                width="100%" height="100%"
+                viewBox={`0 0 ${w} ${h}`}
+            //or show part and let the users pan and zoom
+            // width={w} height={h}
             >
-                
+
                 {this.drawEdges(edges)}
                 {this.drawNodes(nodes)}
             </svg>
