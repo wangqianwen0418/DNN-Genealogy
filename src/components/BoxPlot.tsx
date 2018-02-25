e36a5754aaff790e1d2f2784ebcf9204a1d91ee2
wangqianwen0418 <wangqianwen0418@163.com>
2018-02-25 15:52:29 +0800

    bug fix: outlier

--- src/components/BoxPlot.tsx
+++ src/components/BoxPlot.tsx
@@ -17,21 +17,35 @@ export default class BoxPlot extends React.Component<Props, {}> {
     getQuartiles(datum: number[], percent: number) {
         return d3.quantile(datum, percent) || 0
     }
+    getiqr(k:number, d:number[]) {
+        
+          var q1 = d3.quantile(d, 0.25)||0,
+              q3 = d3.quantile(d, 0.75)||0,
+              iqr = (q3 - q1) * k,
+              i = -1,
+              j = d.length;
+          while (d[++i] < q1 - iqr);
+          while (d[--j] > q3 + iqr);
+          return [i, j];
+      }
     render() {
-        let { height, width, datum, offset, value } = this.props
+        let { height, width, datum, offset, value } = this.props,
+            [i,j]=this.getiqr(1.5, datum),
+            filtered = datum.slice(i,j)
+            
         let boxW = this.scale((this.getQuartiles(datum, 0.75) - this.getQuartiles(datum, 0.25)))
         return <g className="boxplot" transform={`translate(${offset[0]}, ${offset[1]})`}>
             {/* max and min value */}
             <line
                 stroke="gray"
-                x1={0} y1={0} x2={0} y2={height} /> 
+                x1={this.scale(datum[i])} y1={0} x2={this.scale(datum[i])} y2={height} /> 
             <line
                 stroke="gray"
-                x1={width} y1={0} x2={width} y2={height} />
+                x1={this.scale(datum[j])} y1={0} x2={this.scale(datum[j])} y2={height} />
             <line
                 stroke="gray"
-                x1={0} y1={height / 2}
-                x2={width} y2={height / 2} />
+                x1={this.scale(datum[i])} y1={height / 2}
+                x2={this.scale(datum[j])} y2={height / 2} />
             {/* the box */}
             <rect
                 x={this.scale(this.getQuartiles(datum, 0.25))} y={0}
