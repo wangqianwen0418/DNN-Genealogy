8d0d329611f6502ec6b19bfb41272fc5a2406792
wangqianwen0418 <wangqianwen0418@163.com>
2018-03-11 23:10:26 +0800

    modify traning panle

--- src/components/Train.tsx
+++ src/components/Train.tsx
@@ -1,70 +1,76 @@
-import { Menu, Icon, Card } from 'antd';
+import { Tabs, Collapse, Icon, Card } from 'antd';
 import "./Train.css"
 import * as React from "react";
 import axios from 'axios';
 
-const SubMenu = Menu.SubMenu;
+const TabPane = Tabs.TabPane
+const Panel = Collapse.Panel;
 
 
-export interface Props{
-    treeType:string;
-    onSelect:(node:string)=>void
+export interface Props {
+    treeType: string;
+    onSelect: (node: string) => void
 }
-export interface State{
-    menuData: any
+export interface State {
+    tabsData: any,
+    current: string
 }
 
-export default class Sider extends React.Component<Props, State>{
-  // submenu keys of first level
-  constructor(props:Props){
-      super(props)
-      this.state = {
-        menuData:[]
-      }
-  }
-  async getData() {
-    let res = await axios.get('../../data/train.json')
-    this.setState({ menuData: res.data })
-}
-//   onOpenChange = (openKeys:string[]) => {
-//     const latestOpenKey = openKeys.find((key:string) => this.state.openKeys.indexOf(key) === -1)||'';
-//     if (this.rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
-//       this.setState({ openKeys });
-//     } else {
-//       this.setState({
-//         openKeys: latestOpenKey ? [latestOpenKey] : [],
-//       });
-//     }
-//   }
-  componentWillMount(){
-      this.getData()
-  }
-  render() {
-    let menu = this.state.menuData.map((subMenu:any)=>{
-        return <SubMenu key={subMenu.name} title={<span className="sub1">{subMenu.name}</span>}>
-        {subMenu.children.map((menuItem:any)=>{
-            return <SubMenu key={menuItem.name} title={<span className="sub2">{menuItem.name}</span>}>
-                <Menu.Item>
-                    {menuItem.url}
-                </Menu.Item>
-            </SubMenu>
-        })}
-        </SubMenu>
-    })
-    return (
-        <Card 
-        bordered={false}
-        title={<span style={{fontSize:"1.2em"}}>Training</span>} 
-        className="View ViewBottom"
-        >
-      <Menu
-        mode="inline"
-        defaultOpenKeys={[]}
-        // onOpenChange={this.onOpenChange}
-      >
-        {menu}
-      </Menu>
-      </Card>
-    );
-  }
+export default class Train extends React.Component<Props, State>{
+    // submenu keys of first level
+    constructor(props: Props) {
+        super(props)
+        this.state = {
+            tabsData: [],
+            current: ''
+        }
+    }
+    async getData() {
+        let res = await axios.get('../../data/train.json')
+        this.setState({ tabsData: res.data })
+    }
+    //   onOpenChange = (openKeys:string[]) => {
+    //     const latestOpenKey = openKeys.find((key:string) => this.state.openKeys.indexOf(key) === -1)||'';
+    //     if (this.rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
+    //       this.setState({ openKeys });
+    //     } else {
+    //       this.setState({
+    //         openKeys: latestOpenKey ? [latestOpenKey] : [],
+    //       });
+    //     }
+    //   }
+    changeTab(key: string) {
+
+    }
+    componentWillMount() {
+        this.getData()
+    }
+    render() {
+        let panes = this.state.tabsData.map((tab: any, i: number) => {
+            return <TabPane tab={tab.name} key={i} 
+            style={{
+                overflowY:"auto",
+                height:"inherit"
+                }}>
+                <Collapse bordered={false} >
+                {tab.children.map((pane:any)=>{
+                    return <Panel header={pane.name} key={pane.name}>
+                    <p>{pane.url}</p>
+                </Panel>
+                })}
+                </Collapse>
+            </TabPane>
+        })
+        return (
+            <Card
+                bordered={false}
+                title={<span style={{ fontSize: "1.2em" }}>Training</span>}
+                className="View ViewBottom"
+            >
+                <Tabs onChange={this.changeTab} >
+                    {panes}
+                </Tabs>
+            </Card>
+        );
+    }
 }
