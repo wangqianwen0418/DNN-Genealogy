d578231c2cb5d507e8c9d65f6a43832a4a95e22b
wangqianwen0418 <wangqianwen0418@163.com>
2018-07-20 01:38:47 +0800

    new performance compare view

---
+++ public/data/rnn_converter.py
@@ -0,0 +1,17 @@
+import json 
+jsonfile = open('rnn_data.json')
+oldjson = json.load(jsonfile)
+newjson = []
+for item in oldjson:
+    table = item['table']
+    for tab in table:
+        newTab = {}
+        newTab['datasets'] = tab['dataset']
+        newTab['models'] = {}
+        for i, model in enumerate( tab['model'] ):
+            newTab['models'][model] = [d[int(i)] for d in tab['acc']]
+        newjson.append(newTab)
+
+newJsonFile = open('rnn_scores.json', 'w')
+json.dump(newjson, newJsonFile)
+
