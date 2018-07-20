01f364a83685f262e01fc04189a6f82bd90fb85a
wangqianwen0418 <wangqianwen0418@163.com>
2018-07-20 16:05:13 +0800

    some info change

--- public/data/rnn_converter.py
+++ public/data/rnn_converter.py
@@ -6,7 +6,9 @@ for item in oldjson:
     table = item['table']
     for tab in table:
         newTab = {}
+        newTab['name'] = tab['field']
         newTab['datasets'] = tab['dataset']
+        newTab['modelIDs'] = item['ID']
         newTab['models'] = {}
         for i, model in enumerate( tab['model'] ):
             newTab['models'][model] = [d[int(i)] for d in tab['acc']]
