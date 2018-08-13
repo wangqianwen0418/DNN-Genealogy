2361508615fa3335d5f080c1fd99594c471f15a1
wangqianwen0418 <wangqianwen0418@163.com>
2018-08-13 19:33:02 +0800

    params for vgg19

--- public/data/recognition/generate_template.py
+++ public/data/recognition/generate_template.py
@@ -1,14 +1,21 @@
 import json
+import keras
 
-# model is a Keras model object
-json_string = model.to_json()
-summary = json.loads(json_string)
 
-params = {}
-for layer in model.layers:
-    params[layer.name] = layer.count_params()
-summary['params'] = params
+def generate_json(model, filename):
+    json_string = model.to_json()
+    summary = json.loads(json_string)
 
-with open("filename.json", "w") as jsonf:
-    jsonf.write(json.dumps(summary))
-jsonf.close()
+    params = {}
+    for layer in model.layers:
+        params[layer.name] = int(layer.count_params())
+    summary['params'] = params
+
+    with open("{}.json".format(filename), "w") as jsonf:
+        json.dump(summary, jsonf)
+    jsonf.close()
+
+
+model = keras.applications.vgg19.VGG19(include_top=True, weights=None, input_tensor=None, input_shape=None, pooling=None, classes=1000)
+
+generate_json(model, "vgg19")
